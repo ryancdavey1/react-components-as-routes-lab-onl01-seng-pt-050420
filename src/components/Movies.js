128c7f970f923a73d3e587deb8dfe40fb5fa5400
ryancdavey1 <ryancdavey1@gmail.com>
2021-02-04 12:46:36 -0700

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,18 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+       <h1>Movies Page</h1>
+       {movies.map(movie => {
+         return (
+           <div>
+            <h2>{movie.title}</h2>
+            <h3>{movie.time}</h3>
+            <ul>
+            {movie.genres.map(genre => <li>{genre}</li>)}
+            </ul>
+           </div>
+         )
+       })}
     </div>
   );
 };
